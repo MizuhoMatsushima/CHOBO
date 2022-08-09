6e2273aeee13588a72a33d783eacf4e6b51c0e30
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-08-09 14:40:05 +0000

    [Finish]

---
+++ app/controllers/admin/comments_controller.rb
@@ -0,0 +1,10 @@
+class Admin::CommentsController < ApplicationController
+
+  def destroy
+    @consultation = Consultation.find(params[:consultation_id])
+    #@consultation_tags = @consultation.tags
+    @comment = Comment.find(params[:id])
+    @comment.update(body: "このコメントは管理者によって削除されました")
+    redirect_back(fallback_location: root_path)
+  end
+end
