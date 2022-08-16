722183dcf0c7ba8a9e622807181f2b97b1baf319
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-08-16 13:40:07 +0000

    [Update]

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,14 +2,15 @@ class Public::CommentsController < ApplicationController
 
   def create
     @consultation = Consultation.find(params[:consultation_id])
-    @comment = current_end_user.comments.new(consultation_params)
+    @consultation_tags = @consultation.tags
+    @comment = current_end_user.comments.new(comment_params)
     @comment.consultation_id = @consultation.id
     @comment.save
   end
 
   private
 
-  def consultation_params
+  def comment_params
     params.require(:comment).permit(:body)
   end
 end
