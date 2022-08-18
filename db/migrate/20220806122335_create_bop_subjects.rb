dccd134f9dddf47c20f9fdf13a0a09430817272f
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-08-18 12:14:16 +0000

    [Add]

--- db/migrate/20220806122335_create_bop_subjects.rb
+++ db/migrate/20220806122335_create_bop_subjects.rb
@@ -5,13 +5,13 @@ class CreateBopSubjects < ActiveRecord::Migration[6.1]
       t.references :account_book, foreign_key: true
       t.string :subject_name, null: false, default: ""
       t.integer :bop, default: ""
-      t.integer :total_price, null: false, default: ""
-      t.datetime :use_at, null: false, default: ""
-      t.integer :point, default: ""
-      t.string :memo, default: ""
-      t.integer :registered_person_id, default: ""
-      t.string :share_code, default: ""
-      t.integer :share_end_user_id, default: ""
+      t.integer :total_price, null: false
+      t.datetime :use_at, null: false
+      t.integer :point
+      t.string :memo
+      t.integer :registered_person_id
+      t.string :share_code
+      t.integer :share_end_user_id
       t.timestamps
     end
   end
