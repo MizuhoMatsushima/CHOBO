e23735d7ba5429d80cec513c6ef10b56d08ceb65
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-08-31 00:59:33 +0000

    [Update]

--- app/controllers/public/guests_controller.rb
+++ app/controllers/public/guests_controller.rb
@@ -1,5 +1,5 @@
 class Public::GuestsController < ActionController::Base
-  
+
   def guest_sign_in
     end_user = EndUser.find_or_create_by!(email: 'guest@com') do |end_user|
       end_user.password = SecureRandom.urlsafe_base64
@@ -11,7 +11,7 @@ class Public::GuestsController < ActionController::Base
      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
    end
     sign_in end_user
-    redirect_to end_users_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to end_users_path, notice: 'ゲストユーザーとしてログインしました'
   end
 
 end
